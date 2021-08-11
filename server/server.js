abb81a73f1fc71bca90cbd5d9fccf9cf00b0822d
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-08-11 23:14:05 +0800

    initial the project

---
+++ server/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import { readdirSync } from 'fs';
+const morgan = require('morgan');
+
+const app = express();
+
+// DB CONNECTION
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('DB CONNECTED!'))
+  .catch((err) => console.log('DB CANNOT CONNECT BECAUSE::', err));
+
+// middlewares
+app.use(cors());
+app.use(morgan('dev'));
+
+readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`App is listening to port: ${port}`));
